7fa421e080a0d6d75a19b78bfb8a98bfb386df53
bennikabs <bennykabs90@gmail.com>
2023-12-17 19:31:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+export default function App() {
+  let [temperature, setTemperature] = useState();
+  let [description, setDescription] = useState();
+  let [humidity, setHumidity] = useState();
+  let [wind, setWind] = useState();
+  let [result, setResult] = useState(null);
+  let [city, setCity] = useState("");
+  let [icon, setIcon] = useState();
+
+  axios.get("/user/12345", {
+    validateStatus: function (status) {
+      return status < 500; // Resolve only if the status code is less than 500
+    },
+  });
+  function displayWeather(response) {
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+
+  useEffect(() => {
+    if (city) {
+      let apiKey = "c68d952d49c1f7b73f7353b16aa88d42";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+      axios.get(apiUrl).then(displayWeather);
+    }
+  }, [city]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "c68d952d49c1f7b73f7353b16aa88d42";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+    axios.get("/user/12345", {
+      validateStatus: function (status) {
+        return status < 500; // Resolve only if the status code is less than 500
+      },
+    });
+    setResult(
+      <ul>
+        <li>Temperature: {temperature} °C</li>
+        <li>Description: {description}</li>
+        <li>Humidity: {humidity} %</li>
+        <li>Wind: {wind} km/h</li>
+        <li>
+          <img src={icon} alt="weather icon" />
+        </li>
+      </ul>
+    );
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          onChange={updateCity}
+          name="city"
+          placeholder="Enter a city..."
+        />
+        <input type="submit" value="Search" />
+      </form>
+      {result}
     </div>
   );
+  
 }
-
-export default App;
---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import react from "react";
+export default function Footer() {
+  return (
+    <div>
+      <a
+        href="https://github.com/bennikabs/react-weatherapp.git"
+        target="“_blank”"
+      >
+        Open SourceCode
+      </a>
+      by Benny Kabs
+    </div>
+  );
+}
