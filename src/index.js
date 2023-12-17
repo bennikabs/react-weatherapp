75023766f401e1f7c815d52ccfeac8a200c95b88
bennikabs <bennykabs90@gmail.com>
2023-12-17 22:20:47 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.css";
-
-import axios from "axios";
 
 import App from "./App";
 
