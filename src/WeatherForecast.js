b7c59982abc72a9e244ef31bf7ad7d0acb26cb35
bennikabs <bennykabs90@gmail.com>
2023-12-27 03:17:58 +0100

    integrated forecast API
    due to the integration of the api call,  we get the response from the api based on the lattitude and the longtitude of the city that we search for.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "c68d952d49c1f7b73f7353b16aa88d42";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast d-flex justify-content-center text-center">
       <div className="row">
