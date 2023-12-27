a77aa57ec7e2addd1463dedf2944738dc8497828
bennikabs <bennykabs90@gmail.com>
2023-12-27 02:48:13 +0100

    implemented the forecast layout

---
+++ src/WeatherForecast.js
@@ -0,0 +1,20 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast d-flex justify-content-center text-center">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon code="01d" />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
